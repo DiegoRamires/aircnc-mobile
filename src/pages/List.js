d60450647ac94ff48ff1fcc0e45aa2603eb14c2b
DiegoRamires <goncalves.ramires@gmail.com>
2019-10-04 22:52:18 -0300

    styling List route positioning logo image on screen. preparing app to responding to client interested techs

--- src/pages/List.js
+++ src/pages/List.js
@@ -1,8 +1,35 @@
-import React from 'react'
-import { View } from 'react-native'
+import React, { useState, useEffect } from 'react'
+import { SafeAreaView, StyleSheet, Image, AsyncStorage } from 'react-native'
+
+import logo from '../assets/logo.png'
 
 export default function List() {
+  const [techs, setTechs] = useState([])
+
+  useEffect(() => {
+    AsyncStorage.getItem('techs').then(storageTechs => {
+      const techsArray = storageTechs.split(',').map(tech => tech.trim())
+
+      setTechs(techsArray)
+    })
+  }, [])
+
   return (
-    <View />
+    <SafeAreaView style={styles.container} >
+      <Image style={styles.logo} source={logo} />
+    </SafeAreaView>
   )
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+  },
+
+  logo: {
+    height: 32,
+    resizeMode: 'contain',
+    alignSelf: 'center',
+    marginTop: 25,
+  },
+})
